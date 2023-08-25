21551f7a9ea547ae0edbe69d49719b45a5691f6a
Alex Skrypnyk <alex@drevops.com>
2023-08-26 09:27:17 +1000

    Added untested code.

--- hello.sh
+++ hello.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ "$1" == "world" ]]; then
+
+  if [[ -n "$2" ]]; then
+    echo "Hello World $2"
+  else
     echo "Hello World"
+  fi
 else
-    echo "Hello unknown"
+  echo "Hello unknown"
 fi
